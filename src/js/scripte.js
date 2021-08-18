aa97d481d8c58d6c99c87f7ec85c23758ffe1184
Imed Boumalek <imedboumalek@gmail.com>
2021-08-19 00:17:26 +0100

    refactor

--- src/js/scripte.js
+++ src/js/scripte.js
@@ -1,91 +1,44 @@
-import {theme} from './theme.js';
+import {
+    theme
+} from './theme.js';
 theme();
 
 
-const screen = document.querySelector('[data-screen]');
-const number = document.querySelectorAll('[data-number]');
-const operation = document.querySelectorAll('[data-operation]');
+const output = document.querySelector('.num-screen');
+const numbers = document.querySelectorAll('.num-btn .num');
+const btns = document.querySelectorAll('.num-btn');
+const operators = document.querySelectorAll('.num-btn .oper');
 const del = document.querySelector('.del');
-const rest = document.querySelector('[data-rest]');
-const equal = document.querySelector('[data-equal]');
+const rest = document.querySelector('.rest-btn');
+const equal = document.querySelector('.equal-btn');
 
 
-class Calculator {
-    constructor(screen) {
-        this.screen = screen;
-        this.prev = 0;
-        this.current = 0;
-    }
 
-    screenupdate(num) {
-        if (num === '.' && this.screen.innerText.includes('.')) return
-        // this.screen.innerText = this.screen.toString() + number.toString()
-        this.screen.innerText = this.screen.innerText.toString() + num.toString()
-    }
 
-    operation(operate) {
-        this.prev = parseInt(screen.innerText);
-        this.screen.innerText = this.screen.innerText.toString() + operate.toString()
-    }
+btns.forEach((e) => {
+    if (e.classList.contains("num") || e.classList.contains("oper"))
+        e.addEventListener('click', () => {
+            console.log("hi");
 
-    clear() {
-        this.screen.innerText = ''
-        this.operation = undefined
-    }
 
-    delete() {
-        
-    }
-}
+            // let opRegex = RegExp("[+-*/]");
+            // if (opRegex.compile().test(output.innerText[output.innerText.length - 1]))
+            output.innerText += e.innerText;
+        });
 
-const calcul = new Calculator(screen)
-
-number.forEach(e => {
-    e.addEventListener('click', () => {
-        calcul.screenupdate(e.innerText);
-        //console.log(screen.innerText)
-        console.log(parseInt(screen.innerText))
-    })
-})
-
-operation.forEach(e => {
-    e.addEventListener('click', () => {
-        calcul.operation(e.innerText);
-        console.log(parseInt(screen.innerText));
-    })
-})
+});
 
 rest.addEventListener('click', () => {
-    calcul.clear()
-})
+    output.innerText = "";
+});
+
 
 del.addEventListener('click', () => {
-    calcul.delete()
-})
+    output.innerText = output.innerText.slice(0, -1);
+});
 
 
-
-
-// number.forEach(e => {
-//     e.addEventListener('click', () => {
-//         console.log(e.innerText)
-//     })
-// })
-
-// operation.forEach(e => {
-//     e.addEventListener('click', () => {
-//         console.log(e.innerText)
-//     })
-// })
-
-// del.addEventListener('click', () => {
-//     console.log(del.innerText)
-// })
-
-// rest.addEventListener('click', () => {
-//     console.log(rest.innerText)
-// })
-
-// equal.addEventListener('click', () => {
-//     console.log(equal.innerText)
-// })
+equal.addEventListener('click', () => {
+    const result = eval(output.innerText);
+    output.innerText = result;
+});
--- src/js/theme.js
+++ src/js/theme.js
@@ -27,32 +27,32 @@ const gg = document.getElementsByClassName('num-btn');
 
 
 const theme01 = () => {
-        body.style.backgroundColor = 'hsl(222, 26%, 31%)';
-        themeBtn.style.justifyContent = 'flex-start';
-        themeBtn.style.backgroundColor = 'hsl(223, 31%, 20%)';
-        circle.style.backgroundColor = '';
-        //the-head
-        text.style.color = 'white';
-        theme1.style.color = 'white';
-        numberOfTheme.style.color = 'white';
+    body.style.backgroundColor = 'hsl(222, 26%, 31%)';
+    themeBtn.style.justifyContent = 'flex-start';
+    themeBtn.style.backgroundColor = 'hsl(223, 31%, 20%)';
+    circle.style.backgroundColor = '';
+    //the-head
+    text.style.color = 'white';
+    theme1.style.color = 'white';
+    numberOfTheme.style.color = 'white';
 
-        //the screen 
-        theScreen.style.backgroundColor = 'hsl(224, 36%, 15%)';
-        numScreen.style.color = 'white';
-        
-        //the button
-        allBtn.style.backgroundColor = 'hsl(223, 31%, 20%)';
-        del.style.backgroundColor = 'hsl(225, 21%, 49%)';
-        restBtn.style.backgroundColor = 'hsl(225, 21%, 49%)';
+    //the screen 
+    theScreen.style.backgroundColor = 'hsl(224, 36%, 15%)';
+    numScreen.style.color = 'white';
 
-        restBtn.style.color = '';
-        del.style.color = '';
-        equal.style.backgroundColor = '';
+    //the button
+    allBtn.style.backgroundColor = 'hsl(223, 31%, 20%)';
+    del.style.backgroundColor = 'hsl(225, 21%, 49%)';
+    restBtn.style.backgroundColor = 'hsl(225, 21%, 49%)';
 
-        for (var i = 0; i < numButton.length; i++) {
-            numButton[i].style.color = '';
-            numButton[i].style.backgroundColor = '';
-        }
+    restBtn.style.color = '';
+    del.style.color = '';
+    equal.style.backgroundColor = '';
+
+    for (var i = 0; i < numButton.length; i++) {
+        numButton[i].style.color = '';
+        numButton[i].style.backgroundColor = '';
+    }
 };
 
 const theme02 = () => {
@@ -68,7 +68,7 @@ const theme02 = () => {
     //the screen 
     theScreen.style.backgroundColor = 'hsl(0, 0%, 93%)';
     numScreen.style.color = 'hsl(60, 10%, 19%)';
-    
+
     //the button
     allBtn.style.backgroundColor = 'hsl(0, 5%, 81%)';
     del.style.backgroundColor = 'hsl(185, 42%, 37%)';
@@ -88,31 +88,31 @@ const theme02 = () => {
 
 const theme03 = () => {
     body.style.backgroundColor = 'hsl(268, 75%, 9%)';
-        themeBtn.style.justifyContent = 'flex-end';
-        themeBtn.style.backgroundColor = 'hsl(268, 71%, 12%)';
-        circle.style.backgroundColor = 'hsl(176, 100%, 44%)';
-        //the-head
-        text.style.color = 'hsl(52, 100%, 62%)';
-        theme1.style.color = 'hsl(52, 100%, 62%)';
-        numberOfTheme.style.color = 'hsl(52, 100%, 62%)';
+    themeBtn.style.justifyContent = 'flex-end';
+    themeBtn.style.backgroundColor = 'hsl(268, 71%, 12%)';
+    circle.style.backgroundColor = 'hsl(176, 100%, 44%)';
+    //the-head
+    text.style.color = 'hsl(52, 100%, 62%)';
+    theme1.style.color = 'hsl(52, 100%, 62%)';
+    numberOfTheme.style.color = 'hsl(52, 100%, 62%)';
 
-        //the screen 
-        theScreen.style.backgroundColor = 'hsl(268, 71%, 12%)';
-        numScreen.style.color = 'hsl(52, 100%, 62%)';
-        
-        //the button
-        allBtn.style.backgroundColor = 'hsl(268, 71%, 12%)';
-        del.style.backgroundColor = 'hsl(281, 89%, 26%)';
-        del.style.color = 'white';
-        restBtn.style.backgroundColor = 'hsl(281, 89%, 26%)';
-        restBtn.style.color = 'white';
-        equal.style.backgroundColor = 'hsl(176, 100%, 44%)';
-        
-        for (var i = 0; i < numButton.length; i++) {
-            numButton[i].style.color = 'hsl(52, 100%, 62%)';
-            numButton[i].style.backgroundColor = 'hsl(268, 47%, 21%)';
-        }
-}
+    //the screen 
+    theScreen.style.backgroundColor = 'hsl(268, 71%, 12%)';
+    numScreen.style.color = 'hsl(52, 100%, 62%)';
+
+    //the button
+    allBtn.style.backgroundColor = 'hsl(268, 71%, 12%)';
+    del.style.backgroundColor = 'hsl(281, 89%, 26%)';
+    del.style.color = 'white';
+    restBtn.style.backgroundColor = 'hsl(281, 89%, 26%)';
+    restBtn.style.color = 'white';
+    equal.style.backgroundColor = 'hsl(176, 100%, 44%)';
+
+    for (var i = 0; i < numButton.length; i++) {
+        numButton[i].style.color = 'hsl(52, 100%, 62%)';
+        numButton[i].style.backgroundColor = 'hsl(268, 47%, 21%)';
+    }
+};
 
 
 
@@ -120,15 +120,23 @@ let x = 0;
 
 function choice() {
     x++;
-    if (x === 0) {
-        theme01();
-    } else if (x === 1) {
-        theme02();
-    } else if (x === 2) {
-        theme03();
-    } else if (x === 3) {
-        x = 0;
-        theme01();
+    switch (x) {
+
+
+        case 0:
+            theme01();
+            break;
+        case 1:
+            theme02();
+            break;
+        case 2:
+            theme03();
+            break;
+
+        default:
+            x = 0;
+            theme01();
+            break;
     }
 }
 
@@ -136,8 +144,10 @@ function choice() {
 
 function theme() {
     console.log('i coock');
-    themeBtn.addEventListener('click', choice)
+    themeBtn.addEventListener('click', choice);
 }
 
 
-export {theme}
+export {
+    theme
+};
